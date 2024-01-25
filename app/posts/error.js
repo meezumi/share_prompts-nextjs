e171a7ded54d3464a0669bd178220ef9ee6c27e3
aark07 <aaryank098@gmail.com>
2024-01-25 21:26:26 +0530

    data fetching and types

--- app/posts/error.js
+++ app/posts/error.js
@@ -13,6 +13,7 @@ return (
     <h2> something went wrong. </h2>
     <button
       onClick={
+        // attempt to recover by trying to re-render the segment.
         () => reset()
       }
       >
