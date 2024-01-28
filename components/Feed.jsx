e2bd89eac6a94b473253e71f79dd2b5e3a079375
aark07 <aaryank098@gmail.com>
2024-01-28 12:56:58 +0530

    implimenting search (referenced)

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,69 +1,96 @@
-'use client'
-// here we are going to show all the prompts
+"use client";
 
-import { useState, useEffect } from 'react'
-import PromptCard from './PromptCard'
+import { useState, useEffect } from "react";
+
+import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div className='mt-16 prompt_layout'>
+    <div className="mt-16 prompt_layout">
       {data.map((post) => (
-        <PromptCard 
+        <PromptCard
           key={post._id}
           post={post}
-          handleTagClick={handleTagClick}  
+          handleTagClick={handleTagClick}
         />
       ))}
-      {/* but to show the prompts we need to fetch them from our own api */}
     </div>
   );
 };
-// PromptCardList is only going to be used on this specific feed, so we create it outside Feed() function
 
 const Feed = () => {
+  const [allPosts, setAllPosts] = useState([]);
 
-  const [searchText, setSearchText] = useState('');
-  const [posts, setPosts] = useState([]);
+  // Search states
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
 
-  const handleSearchChange = (e) => {
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
 
-  }
+    setAllPosts(data);
+  };
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch("/api/prompt");
-
-      const data = await response.json();
-
-      setPosts(data);
-    };
-
-    console.log(posts);
-
-    fetchPosts(); // calling the above function
+    fetchPosts();
   }, []);
 
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
+
   return (
-    <section className='feed'>
-      <form className='relative w-full flex-center'>
-        <input 
-          type='text'
-          placeholder='Search for Prompts, Tag or Username'
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
           value={searchText}
           onChange={handleSearchChange}
           required
-          className='search_input peer'
+          className="search_input peer"
         />
-
       </form>
 
-      <PromptCardList 
-        data={posts}
-        handleTagClick={() => {}}
-      />
-      
+      {/* All Prompts prompts matching the search field */}
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+      )}
     </section>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
