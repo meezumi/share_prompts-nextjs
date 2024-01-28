dee099bfe6c55628def650936403ad9ccc047e68
aark07 <aaryank098@gmail.com>
2024-01-28 13:44:53 +0530

    TwT

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,48 +1,37 @@
 "use client";
 
 import { useEffect, useState } from "react";
-// this will let us know which user is about to log in
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
 
-import React from "react";
-
-const EditPrompt = () => {
+const UpdatePrompt = () => {
   const router = useRouter();
-
   const searchParams = useSearchParams();
-  const promptId = searchParams.get('id');
+  const promptId = searchParams.get("id");
 
-  const [submitting, setSubmitting] = useState(false);
-  // to check if the prompt is being submitted
-
-  const [post, setPost] = useState({
-    prompt: "",
-    tag: "",
-  });
+  const [post, setPost] = useState({ prompt: "", tag: "" });
+  const [submitting, setIsSubmitting] = useState(false);
 
   useEffect(() => {
     const getPromptDetails = async () => {
-      const response = await fetch(`/api/prompt/${promptId}`)
+      const response = await fetch(`/api/prompt/${promptId}`);
       const data = await response.json();
 
       setPost({
         prompt: data.prompt,
         tag: data.tag,
-      })
-    }
+      });
+    };
 
-    if(promptId) getPromptDetails()
-  }, [promptId])
+    if (promptId) getPromptDetails();
+  }, [promptId]);
 
   const updatePrompt = async (e) => {
-    // e is the event.
     e.preventDefault();
-    // prevents reload of the browser when submitting form
-    setSubmitting(true);
+    setIsSubmitting(true);
 
-    if(!promptId) return alert('Prompt ID not found.')
+    if (!promptId) return alert("Missing PromptId!");
 
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
@@ -59,12 +48,11 @@ const EditPrompt = () => {
     } catch (error) {
       console.log(error);
     } finally {
-      setSubmitting(false);
+      setIsSubmitting(false);
     }
   };
 
   return (
-    // <div>Create Prompt</div>
     <Form
       type="Edit"
       post={post}
@@ -75,4 +63,4 @@ const EditPrompt = () => {
   );
 };
 
-export default EditPrompt;
+export default UpdatePrompt;
