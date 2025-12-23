8195d2c3536466bb69b1cf107cc4b0685a59e444
meezumi <aaryank3000@gmail.com>
2025-12-23 16:05:16 +0530

    Refactor Nav component for improved hover effects and animations

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -33,13 +33,13 @@ const Nav = () => {
 
   return (
     <nav className="flex-between w-full mb-16 pt-3 animate-slideInDown">
-      <Link href="/" className="flex gap-2 flex-center hover:opacity-80 transition-opacity">
+      <Link href="/" className="flex gap-2 flex-center hover:scale-110 transition-transform hover:opacity-80">
         <Image
           src="/favicon.svg"
           alt="Promptmon logo"
           width={28}
           height={28}
-          className="object-contain hover:scale-110 transition-transform"
+          className="object-contain"
         />
         <p className="logo_text">Promptmon</p>
       </Link>
